83a0bd7736af4d7ecfc26df2e673a58f0ec96402
Weidong Xu <weidxu@microsoft.com>
2021-04-06 15:03:14 +0800

    show track2 api-version

--- compare_java_packages.py
+++ compare_java_packages.py
@@ -1,6 +1,7 @@
 import dataclasses
 import logging
 import csv
+import re
 from urllib.request import urlopen
 
 
@@ -28,6 +29,7 @@ def main():
 def process_java_packages_csv():
     sdk_info = {}
 
+    logging.info(f'query csv: {CSV_PATH}')
     csv_data = urlopen(CSV_PATH).read()
     csv_str = csv_data.decode('utf-8')
     csv_reader = csv.DictReader(csv_str.split('\n'), delimiter=',', quotechar='"')
@@ -42,6 +44,7 @@ def process_java_packages_csv():
             sdk_info[sdk].track1 = True
             if not version == '':
                 sdk_info[sdk].track1_stable = True
+            # update version, take the latest
             sdk_info[sdk].track1_api_version = compare_version(namespace.split('.')[-1], sdk_info[sdk].track1_api_version)
         if package.startswith(TRACK2_PACKAGE_PREFIX) and package != TRACK2_PACKAGE_PREFIX + 'parent':
             sdk = package[len(TRACK2_PACKAGE_PREFIX):]
@@ -50,17 +53,59 @@ def process_java_packages_csv():
             sdk_info[sdk].track2 = True
             if not version == '':
                 sdk_info[sdk].track2_stable = True
+            sdk_info[sdk].track2_api_version = get_track2_version(
+                package,
+                row['VersionPreview'] if version == '' else version)
+
+    print('{0: <32}{1: <16}{2: <16}{3: <16}{4: <16}'.format(
+        'service', 'track1', 'track2', 'track1 api', 'track2 api'))
+    print()
 
     for item in sdk_info.values():
-        print('{0: <32}{1: <16}{2: <16}{3: <16}'.format(item.sdk,
-                                                        ('GA' if item.track1_stable else 'beta') if item.track1 else ' ',
-                                                        ('GA' if item.track2_stable else 'beta') if item.track2 else ' ',
-                                                        item.track1_api_version if item.track1_api_version else ' '))
+        print('{0: <32}{1: <16}{2: <16}{3: <16}{4: <16}'.format(
+            item.sdk,
+            ('GA' if item.track1_stable else 'beta') if item.track1 else ' ',
+            ('GA' if item.track2_stable else 'beta') if item.track2 else ' ',
+            item.track1_api_version if item.track1_api_version else ' ',
+            item.track2_api_version if item.track2_api_version else ' '))
+
+
+def get_track2_version(package: str, version: str) -> str or None:
+    if re.match(r'^2.\d{1,2}.\d{1,2}$', version):
+        return 'premium'
+
+    group_url = 'https://repo1.maven.org/maven2/com/azure/resourcemanager'
+    pom_url = f'{group_url}/{package}/{version}/{package}-{version}.pom'
+
+    logging.info(f'query pom: {pom_url}')
+    pom_data = urlopen(pom_url).read()
+    pom_str = pom_data.decode('utf-8')
+
+    matched = re.search(r'Package tag (.+)\.<', pom_str, re.MULTILINE)
+    tag = None
+    if matched:
+        tag = matched.group(1)
+    else:
+        matched = re.search(r'Package tag (.+)\. For ', pom_str, re.MULTILINE)
+        if matched:
+            tag = matched.group(1)
+    if tag:
+        version = tag.replace('package-', '').replace('-', '_').replace('_preview', '')
+
+        # delete prefix before year
+        if re.match(r'.*_20\d{2}_\d{2}', version):
+            loc = version.find('_20')
+            version = version[loc+1:]
+
+        if re.match(r'^\d{4}_\d{2}$', version):
+            version = version + '_01'
+        return version
+    else:
+        return None
 
 
 def compare_version(current_ver: str, exist_ver: str) -> str:
-    if current_ver.endswith('_preview'):
-        current_ver = current_ver[:-len('_preview')]
+    current_ver = current_ver.replace('_preview', '')
 
     if current_ver.startswith('v') and (not exist_ver or current_ver > exist_ver):
         return current_ver[1:]
