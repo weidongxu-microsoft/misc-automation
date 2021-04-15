d02fe6a6d79b95685abad91eaf62ae2719887309
Weidong Xu <weidxu@microsoft.com>
2021-04-15 15:06:09 +0800

    rudimental kpi integrate

--- compare_java_packages.py
+++ compare_java_packages.py
@@ -2,6 +2,7 @@ import dataclasses
 import logging
 import csv
 import re
+import os
 from typing import List
 from urllib.request import urlopen
 
@@ -26,9 +27,9 @@ class SdkInfo:
     track2_api_version: str = None
 
     ring: str = None
-    traffic: int = None
-    completeness: int = None
-    correctness: int = None
+    traffic: float = None
+    completeness: float = None
+    correctness: float = None
 
     def to_row(self) -> List[str]:
         return [self.sdk,
@@ -38,9 +39,20 @@ class SdkInfo:
                 self.track2_api_version]
 
 
+@dataclasses.dataclass
+class KpiInfo:
+    namespace: str
+    name: str
+    ring: str
+    traffic: float = None
+    completeness: float = None
+    correctness: float = None
+
+
 def run():
     logging.basicConfig(level=logging.INFO)
     sdk_info_list = process_java_packages_csv()
+    join_kpi_csv(sdk_info_list)
     print_stdout(sdk_info_list)
     write_csv(sdk_info_list)
 
@@ -49,9 +61,10 @@ def process_java_packages_csv() -> List[SdkInfo]:
     sdk_info = {}
 
     logging.info(f'query csv: {CSV_PATH}')
-    csv_data = urlopen(CSV_PATH).read()
-    csv_str = csv_data.decode('utf-8')
-    csv_reader = csv.DictReader(csv_str.split('\n'), delimiter=',', quotechar='"')
+    with urlopen(CSV_PATH) as csv_response:
+        csv_data = csv_response.read()
+        csv_str = csv_data.decode('utf-8')
+        csv_reader = csv.DictReader(csv_str.split('\n'), delimiter=',', quotechar='"')
     for row in csv_reader:
         package = row['Package']
         namespace = row['GroupId']
@@ -83,6 +96,41 @@ def process_java_packages_csv() -> List[SdkInfo]:
     return sdk_info_list
 
 
+def join_kpi_csv(sdk_info_list: List[SdkInfo]):
+    if not os.path.isfile(SWAGGER_METADATA_FILENAME) or not os.path.isfile(KPI_LAST_30D_FILENAME):
+        return
+
+    swagger_info = {}
+    with open(SWAGGER_METADATA_FILENAME, 'r', newline='', encoding='utf-8') as f:
+        csv_reader = csv.DictReader(f, delimiter=',', quotechar='"')
+        for row in csv_reader:
+            namespace = row['ProviderNamespace'].strip().upper()
+            if namespace.startswith('MICROSOFT.'):
+                swagger_info[row['specRootFolderName']] = namespace
+
+    kpi_info = {}
+    with open(KPI_LAST_30D_FILENAME, 'r', newline='', encoding='utf-8') as f:
+        csv_reader = csv.DictReader(f, delimiter=',', quotechar='"')
+        for row in csv_reader:
+            namespace = row['resourceProviderName'].strip().upper()
+            if namespace.startswith('MICROSOFT.'):
+                kpi_item = KpiInfo(namespace, row['ServiceName'].strip(), row['ServiceRing'].strip(),
+                                   float(row['trafficCount']), float(row['completeness']), float(row['correctness']))
+                if namespace not in kpi_info:
+                    kpi_info[namespace] = []
+                kpi_info[namespace].append(kpi_item)
+
+    for item in sdk_info_list:
+        if item.sdk in swagger_info:
+            namespace = swagger_info[item.sdk]
+            if namespace in kpi_info:
+                kpi_items = kpi_info[namespace]
+                item.ring = kpi_items[0].ring
+                item.traffic = kpi_items[0].traffic
+                item.completeness = kpi_items[0].completeness
+                item.correctness = kpi_items[0].correctness
+
+
 def print_stdout(sdk_info_list: List[SdkInfo]):
     known_track1_alias = ['cosmosdb',       # cosmos
                           'eventhub',       # eventhubs
@@ -106,16 +154,20 @@ def print_stdout(sdk_info_list: List[SdkInfo]):
     print()
 
     print('{0: <32}{1: <16}{2: <16}{3: <16}{4: <16}'.format(
-        'service', 'track1', 'track2', 'track1 api', 'track2 api'))
+        'service', 'track1', 'track2', 'track1 api', 'track2 api', 'ring', 'traffic', 'completeness', 'coverage'))
     print()
 
+    xround = lambda r, n: None if r is None else round(r, n)
+    xstr = lambda s: '' if s is None else str(s)
+
     for item in sdk_info_list:
-        print('{0: <32}{1: <16}{2: <16}{3: <16}{4: <16}'.format(
+        print('{0: <32}{1: <16}{2: <16}{3: <16}{4: <16}{5: <16}{6: <16}{7: <16}{8: <16}'.format(
             item.sdk,
-            ('GA' if item.track1_stable else 'beta') if item.track1 else ' ',
-            ('GA' if item.track2_stable else 'beta') if item.track2 else ' ',
-            item.track1_api_version if item.track1_api_version else ' ',
-            item.track2_api_version if item.track2_api_version else ' '))
+            ('GA' if item.track1_stable else 'beta') if item.track1 else '',
+            ('GA' if item.track2_stable else 'beta') if item.track2 else '',
+            xstr(item.track1_api_version), xstr(item.track2_api_version),
+            xstr(item.ring), xstr(xround(item.traffic, 0)),
+            xstr(xround(item.completeness, 2)), xstr(xround(item.correctness, 2))))
 
 
 def write_csv(sdk_info_list: List[SdkInfo]):
