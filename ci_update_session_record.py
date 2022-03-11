2041317ed326253a4d629b48b2e989912f0a1239
Weidong Xu <weidxu@microsoft.com>
2022-03-11 14:39:01 +0800

    use main

--- ci_update_session_record.py
+++ ci_update_session_record.py
@@ -5,9 +5,9 @@ import logging
 
 SDK_REPO = 'c:/github/azure-sdk-for-java/sdk/resourcemanager'
 # SDK_REPO = 'c:/github/azure-libraries-for-java'
-RESOURCE_PROVIDER = 'Microsoft.ContainerRegistry'
+RESOURCE_PROVIDER = 'Microsoft.Cache'
 VERSION_CHANGES = {
-    '2019-05-01': '2019-06-01-preview'
+    '2020-12-01': '2021-06-01'
 }
 
 
