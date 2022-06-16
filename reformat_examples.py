fc71e259c7c188093add6faecb0db7c6d40fb19a
Weidong Xu <weidxu@microsoft.com>
2022-06-16 11:49:09 +0800

    update reformat script

--- reformat_examples.py
+++ reformat_examples.py
@@ -1,5 +1,6 @@
+import json
 import os
-
+import re
 
 EXAMPLE_REPO = 'c:/github/azure-rest-api-specs-examples/specification/'
 
@@ -8,21 +9,38 @@ def update_markdown(filepath: str):
     with open(filepath, encoding='utf-8') as f:
         lines = f.readlines()
 
-    code_start = None
+    code_end = None
     for index, line in enumerate(lines):
-        if line.startswith(r'```'):
-            code_start = index
+        if line == '```\n':
+            code_end = index
             break
 
-    if code_start:
-        out_lines = lines[code_start:]
-        out_lines.append('\n')
-        out_lines.extend(lines[:code_start])
-        assert out_lines[-1].strip() == ''
-        out_lines = out_lines[:-1]
+    if code_end:
+        ext = None
+        if 'examples-java' in filepath:
+            ext = '.java'
+        if 'examples-js' in filepath:
+            ext = '.js'
+        if 'examples-go' in filepath:
+            ext = '.go'
 
-        with open(filepath, 'w', encoding='utf-8') as f:
-            f.write(''.join(out_lines))
+        if ext:
+            code_lines = lines[1:code_end]
+
+            ref_lines = lines[code_end+1:]
+            match = re.search(r'\[SDK documentation]\((.*?\.md)\)', ''.join(ref_lines))
+            if match:
+                with open(filepath.replace('.md', ext), 'w', encoding='utf-8') as f:
+                    f.write(''.join(code_lines))
+
+                ref_json = {'sdkUrl': match.group(1)}
+                with open(filepath.replace('.md', '.json'), 'w', encoding='utf-8') as f:
+                    json.dump(ref_json, f)
+
+                os.remove(filepath)
+                return
+
+    print("failed to process " + filepath)
 
 
 def main():
